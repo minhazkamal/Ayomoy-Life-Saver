6ce78e87e6336fac9c0746f0c2e168b4550d3da3
Minhaz Kamal <70626937+minhazkamal@users.noreply.github.com>
2020-11-27 00:30:38 +0600

    Donee New Request Updation

--- src/sample/DoneeNewReqController.java
+++ src/sample/DoneeNewReqController.java
@@ -8,12 +8,14 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.*;
 
 public class DoneeNewReqController {
     ObservableList<String> BGList = FXCollections.observableArrayList("A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-");
@@ -54,14 +56,56 @@ public class DoneeNewReqController {
     }
 
    public void submitNewReq(ActionEvent even) {
-        System.out.println("Name: "+name.getText());
-        System.out.println("Mobile: "+mobile.getText());
-        System.out.println("Location: "+ doneeLocation.getText());
-        System.out.println("Complications: "+complications.getText());
-        System.out.println("Blood Group: "+BG_choice.getValue().toString());
-        System.out.println("Quantity: "+quantity.getText()+" Bag(s)");
-        System.out.println("Approximate Date: "+date.getValue());
-        System.out.println("Emergency Contact Name: "+emContact.getText());
-        System.out.println("Emergency Mobile: "+emMobile.getText());
+//        System.out.println("Name: "+name.getText());
+//        System.out.println("Mobile: "+mobile.getText());
+//        System.out.println("Location: "+ doneeLocation.getText());
+//        System.out.println("Complications: "+complications.getText());
+//        System.out.println("Blood Group: "+BG_choice.getValue().toString());
+//        System.out.println("Quantity: "+quantity.getText()+" Bag(s)");
+//        System.out.println("Approximate Date: "+date.getValue());
+//        System.out.println("Emergency Contact Name: "+emContact.getText());
+//        System.out.println("Emergency Mobile: "+emMobile.getText());
+       String username = "als";
+       String password = "iutcse18";
+       String url = "jdbc:oracle:thin:@localhost:1521/XE";
+       String query = "INSERT INTO NEW_DONEE (NAME, MOBILE, BLOOD_GROUP, LOCATION, COMPLICATIONS, QUANTITY, APPROX_DATE, EM_PERSON, EM_MOBILE) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+       PreparedStatement pst = null;
+       ResultSet rs = null;
+       try{
+           Class.forName("oracle.jdbc.driver.OracleDriver");
+           Connection con = DriverManager.getConnection(url,username,password);
+
+           pst = con.prepareStatement(query);
+           pst.setString(1, name.getText());
+           pst.setString(2, mobile.getText());
+           pst.setString(3, BG_choice.getValue().toString());
+           pst.setString(4, doneeLocation.getText());
+           pst.setString(5, complications.getText());
+           pst.setInt(6, Integer.parseInt(quantity.getText()));
+           pst.setDate(7, Date.valueOf(date.getValue()));
+           pst.setString(8, emContact.getText());
+           pst.setString(9, emMobile.getText());
+
+           pst.executeUpdate();
+
+           Alert alert = new Alert(Alert.AlertType.INFORMATION);
+           alert.setTitle("Request Confirmation");
+           alert.setHeaderText(null);
+           alert.setContentText("Request Successfully Received");
+
+           alert.showAndWait();
+
+       } catch (Exception e)
+       {
+//           System.out.println(e);
+           Alert alert = new Alert(Alert.AlertType.ERROR);
+           alert.setTitle("Error Dialog");
+           alert.setHeaderText("Look, an Error Dialog");
+           alert.setContentText(String.valueOf(e));
+
+           alert.showAndWait();
+       }
+
     }
 }
